8a8a5439d8a8f94aa1e59f8c5394b735293886b6
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2024-01-08 07:56:15 +1100

    Update

---
+++ README.md
@@ -0,0 +1,10 @@
+# HideDiscordChat
+
+HideDiscordChat is a Fabric mod designed for EarthMC. If you like to `/ch l global` but Discord chat messages get in your way, this mod is for you.
+
+The mod is extremely simple with only 3 settings:
+
+- A toggle to enable or disable the mod
+- A list of ranks to filter, defaults to "*" meaning it will filter all ranks
+- A toggle to make the rank filter an exclusive list, meaning any ranks written will not be filtered
+  - This allows you to for example: write "Helper,Mod,Staff Manager,Admin,Owner,Developer" and only see staff messages
--- src/main/java/com/fwloopins/hidediscordchat/client/HideDiscordChatClient.java
+++ src/main/java/com/fwloopins/hidediscordchat/client/HideDiscordChatClient.java
@@ -1,38 +1,24 @@
 package com.fwloopins.hidediscordchat.client;
 
+import com.fwloopins.hidediscordchat.client.config.HDCConfig;
+import com.fwloopins.hidediscordchat.client.manager.FilterManager;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
-import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
-import net.minecraft.util.Formatting;
-
-import java.util.List;
 
 public class HideDiscordChatClient implements ClientModInitializer {
+    private static HDCConfig config = null;
 
     @Override
     public void onInitializeClient() {
-        ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
-            List<Text> siblings = message.getSiblings();
-            if (siblings.size() < 4) return true;
+        AutoConfig.register(HDCConfig.class, GsonConfigSerializer::new);
+        config = AutoConfig.getConfigHolder(HDCConfig.class).getConfig();
 
-            Text indexOne = siblings.get(1);
-            if (!indexOne.getString().equals("[")) return true;
-            TextColor indexOneColour = indexOne.getStyle().getColor();
-            if (indexOneColour == null) return true;
-            if (!indexOneColour.equals(TextColor.fromFormatting(Formatting.GRAY))) return true;
+        FilterManager filterManager = new FilterManager();
+        filterManager.allowGameListener();
+    }
 
-            Text indexTwo = siblings.get(2);
-            if (!indexTwo.getString().equals("Discord")) return true;
-            TextColor indexTwoColour = indexTwo.getStyle().getColor();
-            if (indexTwoColour == null) return true;
-            if (!indexTwoColour.equals(TextColor.fromFormatting(Formatting.AQUA))) return true;
-
-            Text indexThree = siblings.get(3);
-            if (!indexThree.getString().split(" ")[0].equals("]")) return true;
-            TextColor indexThreeColour = indexThree.getStyle().getColor();
-            if (indexThreeColour == null) return true;
-            return !indexThreeColour.equals(TextColor.fromFormatting(Formatting.GRAY));
-        });
+    public static HDCConfig getConfig() {
+        return config;
     }
 }
